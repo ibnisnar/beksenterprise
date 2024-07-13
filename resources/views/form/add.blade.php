3fa27fcdccf051e17283542063aab811abc0d045
Muhammad Tantowi bin Isnar <offcltntwi@gmail.com>
2024-07-13 14:13:30 +0800

    2:13 PM

--- resources/views/form/add.blade.php
+++ resources/views/form/add.blade.php
@@ -30,7 +30,7 @@
                             <label for="product_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product</label>
                             <select id="product_id" name="product_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                                 @foreach($products as $product)
-                                    <option value="{{ $product->id }}">{{ $product->name }}</option>
+                                    <option value="{{ $product->id }}">{{ $product->name }} - {{ $product->quantity }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -50,12 +50,14 @@
                                     </svg>
                                 </button>
                                 <input type="number" id="quantity" name="quantity" data-input-counter aria-describedby="helper-text-explanation" class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" required>
+
                                 <button type="button" id="increment-button" data-input-counter-increment="quantity" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-r-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                                     <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
                                     </svg>
                                 </button>
                             </div>
+                            <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Must not exceed product quantity</p>
                         </div>
                         <div class="mb-5">
                             <label for="discount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Discount (RM)</label>
